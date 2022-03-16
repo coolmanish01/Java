dbd145fb1b63432965cdb54731c8333c3c99c20a
Manish Kumar Santoshi <manishkumarsantoshi@gmail.com>
2022-03-16 09:20:52 +0530

    Add files via upload

---
+++ StringAppend.java
@@ -0,0 +1,7 @@
+class StringAppend{  
+	public static void main(String args[]){  
+		StringBuffer sb=new StringBuffer("Hello ");  
+		sb.append("Java");//now original string is changed  
+		System.out.println(sb);//prints Hello Java  
+	}  
+}  
