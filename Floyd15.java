47e8636d83ad7182481d2584aa7318e3487b8d90
Manish Kumar Santoshi <manishkumarsantoshi@gmail.com>
2022-03-22 07:55:30 +0530

    Create Floyd15.java

---
+++ Floyd15.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Floyd{
+	public static void main(String arg[]){
+		int i,j,k=1,n=6;
+		for(i=1;i<=n;i++){
+			for(j=1;j<=i;j++){
+				System.out.print(k+" ");
+				k++;
+			}
+		System.out.println();
+		}
+	}
+}
